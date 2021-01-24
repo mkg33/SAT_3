83fd220b74d100eb03098028613e318066a5b33d
mkg33 <maja.k.gwozdz@gmail.com>
2021-01-24 16:58:14 +0100

    Added formatting

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 Project 3
 
-Deadline: 15.03.2021
+Deadline: 15.03.2021 (2:00 PM)
 
-UPDATE (24.01)
+**UPDATE (24.01)**
 
 As of today, our solver can solve almost all test cases well within the 1m CPU time limit. There's only one timeout (hole9.cnf). All tests performed with VSIDS.
 There's also one borderline case that takes ca 57-58s but doesn't exceed the limit.
@@ -13,8 +13,8 @@ I'll post the full results soon. It's promising that VSIDS actually outperforms 
 TODO:
 
 * clause deletion strategy (P)
-* VSIDS / VMTF branching heuristic (M)
+* VSIDS / VMTF branching heuristic (M) :white_check_mark:
 * restarts + phase saving + different restart policies (P)
-* preprocessing techniques (M)
-* proof logging in DRUP (or DRAT) format
-* tests
+* preprocessing techniques (M) :white_check_mark: [might add more]
+* proof logging in DRUP (or DRAT) format (M) :white_check_mark:
+* tests + cactus plots (M)
--- maphMain.cpp
+++ maphMain.cpp
@@ -12,7 +12,8 @@ void printError(char * prog) {
 ██║╚██╔╝██║██╔══██║██╔═══╝ ██╔══██║╚════██║██║   ██║██║     ╚██╗ ██╔╝██╔══╝  ██╔══██╗
 ██║ ╚═╝ ██║██║  ██║██║     ██║  ██║███████║╚██████╔╝███████╗ ╚████╔╝ ███████╗██║  ██║
 ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝     ╚═╝  ╚═╝╚══════╝ ╚═════╝ ╚══════╝  ╚═══╝  ╚══════╝╚═╝  ╚═╝
-)" << "\nUsage: " << prog << " <DIMACS file>" << " selection heuristic:\n <FIRST=0 | RANDOM=1 | DLIS=2 | RDLIS=3 | DLCS=4 | RDLCS=5 | JW=6 | RJW=7 | MOMS=8 | RMOMS=9 | VSIDS=10>" << " optional DRAT proof logging: <Y|N>\n\n"
+)"  << "\nUsage: " << prog << " <DIMACS file>" << " selection heuristic:\n <FIRST=0 | RANDOM=1 | DLIS=2 | RDLIS=3 | DLCS=4 | RDLCS=5 | JW=6 | RJW=7 | MOMS=8 | RMOMS=9 | VSIDS=10>"
+    << " optional DRAT proof logging: <Y|N>\n\n"
     << "Available selection heuristics: \n" << "- FIRST: select the first available literal\n"
     << "- RANDOM: select a random literal\n" << "- DLIS: Dynamic Largest Individual Sum\n" << "- RDLIS: randomized Dynamic Largest Individual Sum\n"
     << "- DLCS: Dynamic Largest Combined Sum\n" << "- RDLCS: randomized Dynamic Largest Combined Sum\n" << "- JW: Jeroslow-Wang heuristic\n"
